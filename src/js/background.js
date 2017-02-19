05a222bdbadf21145fd276c4fdd737250692d32f
Tom Rochette <roctom@gmail.com>
2017-02-19 18:52:03 -0500

    [#3] Tab clustering not displayed if already opened Focus on window and tab containing the extension.

--- src/js/background.js
+++ src/js/background.js
@@ -3,6 +3,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.query(toCreate, function(tabs) {
 		if (tabs.length === 0) {
 			chrome.tabs.create(toCreate);
+		} else {
+			var tab = tabs[0];
+			chrome.tabs.update(tab.id, { active: true });
+			chrome.windows.update(tab.windowId, { focused: true });
 		}
 	});
 });
